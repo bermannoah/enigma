42b8f75b59317eebb101113ce4897359fa119067
Tessa Taylor <nots55@yahoo.com>
2016-08-27 10:10:47 -0600

    Updated key generator w/ variables initialized

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,7 +1,11 @@
 class KeyGenerator
-  key = rand(1000..9999).to_s
-  key_a = key[0,1].to_i
-  key_b = key[1,2].to_i
-  key_c = key[2,3].to_i
-  key_d = key[3,4].to_i
+  attr_reader :key
+  attr_accessor :key_a, :key_b, :key_c, :key_d
+
+  def initialize(key=rand(1000..9999).to_s)
+  @key_a = key[0,1].to_i
+  @key_b = key[1,2].to_i
+  @key_c = key[2,3].to_i
+  @key_d = key[3,4].to_i
+  end
 end
