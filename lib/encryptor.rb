efe0a2f49193c02692eab7da9a973c32f0b7159d
Noah Berman <noah@noahberman.org>
2016-08-29 15:34:05 -0600

    THE DECRYPTOR WORKS

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Encryptor < Cipher
 
-  attr_reader :cipher, :rotate, :encrypt
+  attr_reader :cipher, :rotate, :encrypt, :decrypt_rotation_A, :decrypt_rotation_B, :decrypt_rotation_C, :decrypt_rotation_D, :decrypted
   attr_accessor :encrypted, :rotation_count
 
   def initialize
@@ -13,12 +13,17 @@ class Encryptor < Cipher
     @rotation_B = rotation(cipher.key.keyB)
     @rotation_C = rotation(cipher.key.keyC)
     @rotation_D = rotation(cipher.key.keyD)
-    @rotation_count = 1
+    @decrypt_rotation_A = rotation_A.invert
+    @decrypt_rotation_B = rotation_B.invert
+    @decrypt_rotation_C = rotation_C.invert
+    @decrypt_rotation_D = rotation_D.invert
+    @rotation_count = nil
   end
 
   def encrypt(input)
     @input = input
     @encrypted = []
+    @rotation_count = 1
     letters = input.chars.to_a
     letters.each do |letter|
       if @rotation_count == 1
@@ -38,7 +43,37 @@ class Encryptor < Cipher
   end
 
   def print_info
-    puts "Message: #{encrypted.join}..end..,\n Date: #{cipher.date_finder}, Key: #{cipher.key.key} "
+    puts "Message: #{encrypted.join}\n Date: #{cipher.date_finder}, Key: #{cipher.key.key} "
   end
+
+  def decrypt(input)
+    @input = input
+    @decrypted = []
+    @rotation_count = 1
+    letters = input.chars.to_a
+    letters.each do |letter|
+      if @rotation_count == 1
+        @decrypted << decrypt_rotation_A[letter]
+        @rotation_count += 1
+      elsif @rotation_count == 2
+        @decrypted << decrypt_rotation_B[letter]
+        @rotation_count += 1
+      elsif @rotation_count == 3
+        @decrypted << decrypt_rotation_C[letter]
+        @rotation_count += 1
+      elsif @rotation_count == 4
+        @decrypted << decrypt_rotation_D[letter]
+        @rotation_count = 1
+      end
+    end
+  end
+
+
+  def print_decrypted_info
+    puts "Decrypted message: #{decrypted.join}\n Date: #{cipher.date_finder}, Key: #{cipher.key.key} "
+  end
+
+
+
 
 end
